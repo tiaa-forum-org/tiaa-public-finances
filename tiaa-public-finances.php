72b80598c1cf0d7a02a7584692864d6206ea9b39
Josh Reisner <josh@joshreisner.com>
2019-02-09 16:01:58 -0800

    first basic table with aggregated data

---
+++ tiaa-public-finances.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Plugin Name: TiAA Public Finances Report
+ */
+
+add_shortcode('tiaa-public-finances', function(){
+	global $wpdb;
+
+	//fetch payments
+	$payments = $wpdb->get_results('SELECT amount, MONTH(created_at) month, YEAR(created_at) year FROM ' . $wpdb->prefix . 'frm_payments WHERE status = "complete" ORDER BY created_at DESC');
+
+	//loop through and build aggregates
+	$months = array();
+	$total = array('sum' => 0, 'count' => 0);
+	foreach ($payments as $payment) {
+		$key = $payment->year . '-' . $payment->month;
+		if (!array_key_exists($key, $months)) $months[$key] = array('sum' => 0, 'count' => 0);
+		$total['count']++;
+		$months[$key]['count']++;
+		$total['sum'] += $payment->amount;
+		$months[$key]['sum'] += $payment->amount;
+	}
+
+	return '
+	<style>
+		table#tiaa-public-finances th, td { text-align: right; }
+		table#tiaa-public-finances th:first-child, td:first-child { text-align: left; }
+		table#tiaa-public-finances tfoot td { 
+			border-top: 2px solid #bbb;
+			border-bottom: 0;
+			font-weight: bold;
+		}
+	</style>
+	<table id="tiaa-public-finances">
+		<thead>
+			<tr>
+				<th>Month</th>
+				<th># Contributions</th>
+				<th>Total</th>
+			</tr>
+		</thead>
+		<tbody>
+		' . implode(array_map(function($key) use($months){
+			list($year, $month) = explode('-', $key);
+			return '<tr>
+				<td>' . date('F Y', mktime(1, 1, 1, $month, 1, $year)) . '</td>
+				<td>' . number_format($months[$key]['count']) . '</td>
+				<td>$' . number_format($months[$key]['sum'], 2) . '</td>
+			</tr>';
+		}, array_keys($months))) . '
+		</tbody>
+		<tfoot>
+			<tr>
+				<td>Total</td>
+				<td>' . number_format($total['count']) . '</td>
+				<td>$' . number_format($total['sum'], 2) . '</td>
+			</tr>
+		</tfoot>
+	</table>';
+});
